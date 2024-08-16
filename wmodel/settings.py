f7e6261947d1c598e9e0aacb92f17e96a2f80ba2
Priyanshu Jha <priyanshu.k4444@gmail.com>
2024-08-16 12:37:08 +0530

    deploy config

--- wmodel/settings.py
+++ wmodel/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-qx364hiw)&7l2p^1p)$+z0o$^+f-ov$9*%-u3=_%v=x8$s3w(n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["wmodel.onrender.com"]
 
 
 # Application definition
@@ -76,11 +76,9 @@ WSGI_APPLICATION = 'wmodel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
